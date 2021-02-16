a3b52ee5eca014099818b30cba06cac9dc3f44a4
User8956 <11wertgh77@mail.ru>
2021-02-16 10:04:53 +0300

    resurrected_launchable

---
+++ Apps/HightechAngular.Web/Areas/Admin/GetAllOrders/GetAllOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿using Force.Cqrs;
+using HightechAngular.Areas.Shared;
+
+namespace HightechAngular.Areas.Admin.GetAllOrders
+{
+    public class GetAllOrdersQuery : FilterQuery<OrderListItem>
+    {
+        
+    }
+}
